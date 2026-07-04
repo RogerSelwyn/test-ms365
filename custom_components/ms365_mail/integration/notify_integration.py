c1406f8bbb524892ef6d8f768334846e80ee93e0
RogerSelwyn <rogers@selwyn.org.uk>
2026-07-04 15:49:11 +0100

    Test

--- custom_components/ms365_mail/integration/notify_integration.py
+++ custom_components/ms365_mail/integration/notify_integration.py
@@ -29,6 +29,7 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_integration_get_service(hass, config, discovery_info=None):  # pylint: disable=unused-argument
     """Get the service."""
+    _LOGGER.info(discovery_info)
     if (
         discovery_info is None
         or not hasattr(discovery_info[CONF_ENTRY], "runtime_data")
