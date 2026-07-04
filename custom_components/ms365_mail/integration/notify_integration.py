303b8280b9f843b8e0d0a5b73c453d3ec1f0daf4
RogerSelwyn <rogers@selwyn.org.uk>
2026-07-04 16:17:53 +0100

    Remove prints

--- custom_components/ms365_mail/integration/notify_integration.py
+++ custom_components/ms365_mail/integration/notify_integration.py
@@ -29,8 +29,6 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_integration_get_service(hass, config, discovery_info=None):  # pylint: disable=unused-argument
     """Get the service."""
-    print("@@@@@@@@@@@@@@@@@")
-    print(discovery_info)
     if (
         discovery_info is None
         or not hasattr(discovery_info[CONF_ENTRY], "runtime_data")
--- custom_components/ms365_mail/integration/setup_integration.py
+++ custom_components/ms365_mail/integration/setup_integration.py
@@ -15,8 +15,6 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_do_setup(hass: HomeAssistant, entry: ConfigEntry, account):
     """Run the setup after we have everything configured."""
-    print("$$$$$$$$$$$$$$$$$$$$$")
-    print(entry.data)
     _LOGGER.debug("Sensor setup - start")
     email_coordinator = MS365SensorCoordinator(hass, entry, account)
     await email_coordinator.async_config_entry_first_refresh()
@@ -26,8 +24,6 @@ async def async_do_setup(hass: HomeAssistant, entry: ConfigEntry, account):
 
 async def async_extra_platform_setup(hass: HomeAssistant, entry: ConfigEntry):
     """Setup notify platform."""
-    print("£££££££££££££")
-    print(entry.data)
     if entry.data[CONF_ENABLE_UPDATE]:
         hass.async_create_task(
             discovery.async_load_platform(
